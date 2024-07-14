5ed4bc0a199fa49678b81de70051c88af90f2b67
Pangho04 <dlckdgh741@naver.com>
2024-07-14 15:27:39 +0900

    Add: 프로젝트 초기 세팅
    - 각종 세팅 파일 추가
    - 초기 스타일 수정
    - utils 폴더 생성
     - 상태관리 파일 생성
     - 스타일 파일 생성

---
+++ .eslintrc.cjs
@@ -0,0 +1,74 @@
+const globals = require("globals");
+const pluginJs = require("@eslint/js");
+const pluginReactConfig = require("eslint-plugin-react/configs/recommended.js");
+
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    es2022: true,
+  },
+  extends: [
+    "airbnb",
+    "plugin:react/recommended",
+    "plugin:prettier/recommended",
+  ],
+  ignorePatterns: ["dist", ".eslintrc.cjs", "*.spec.jsx"],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [".eslintrc.{js,cjs}", "./tailwind.config.js"],
+      parserOptions: {
+        sourceType: "module",
+      },
+    },
+    {
+      files: ["**/*.jsx"],
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "prettier", "react-hooks"],
+  rules: {
+    semi: "warn",
+    "no-unused-vars": "warn",
+    "import/no-extraneous-dependencies": "off",
+    "react/prop-types": "off",
+    "react/button-has-type": "off",
+    "react/jsx-props-no-spreading": "off",
+    "react/jsx-no-bind": "off",
+    "react/self-closing-comp": "off",
+    "react/react-in-jsx-scope": "off",
+    "react/require-default-props": "warn",
+    "react/jsx-filename-extension": ["warn", { extensions: [".js", ".jsx"] }],
+    "no-param-reassign": 0,
+    "global-require": 0,
+    "no-underscore-dangle": "off",
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  globals: {
+    ...globals.browser,
+    Office: "readonly",
+    Excel: "readonly",
+    Word: "readonly",
+    OneNote: "readonly",
+    PowerPoint: "readonly",
+    Outlook: "readonly",
+  },
+};
---
+++ src/taskpane/utils/style.js
@@ -0,0 +1,10 @@
+import { tokens, makeStyles } from "@fluentui/react-components";
+
+const useStyles = makeStyles({
+  root: {
+    minHeight: "100vh",
+    backgroundColor: tokens.colorNeutralBackground3,
+  },
+});
+
+export default useStyles;
