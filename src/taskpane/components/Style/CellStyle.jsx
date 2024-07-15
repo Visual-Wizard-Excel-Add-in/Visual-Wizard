d51e6c10d43f8267e60e5904949c132a9ece2f57
Pangho04 <dlckdgh741@naver.com>
2024-07-15 23:17:16 +0900

    Feat: CellStyle 기능 탭 구현
    - Style 카테고리의 CellStyle 기능 탭을 구현하였습니다.

---
+++ src/taskpane/components/Style/CellStyle.jsx
@@ -0,0 +1,30 @@
+import { Select, useId } from "@fluentui/react-components";
+import { useStyles } from "../../utils/style";
+import CELL_STYLE_PRESETS from "../../utils/CellStylePresets";
+import { SaveIcon, EditIcon } from "../../utils/icons";
+
+function CellStyle() {
+  const selectId = useId();
+  const styles = useStyles();
+
+  return (
+    <div className="flex items-center justify-between space-x-5">
+      <span>서식 프리셋</span>
+      <div className="flex items-center space-x-2">
+        <Select id={selectId} className="w-24">
+          {CELL_STYLE_PRESETS.map((preset) => (
+            <option key={preset.num}>{preset.num}</option>
+          ))}
+        </Select>
+        <button className={styles.buttons} aria-label="save">
+          <EditIcon />
+        </button>
+        <button className={styles.buttons} aria-label="edit">
+          <SaveIcon />
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default CellStyle;
