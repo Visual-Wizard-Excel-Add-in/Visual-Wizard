53a9b4ef53b406f8bbdbc10d4897ba544fd2dec0
Pangho04 <dlckdgh741@naver.com>
2024-07-15 17:26:43 +0900

    Add: 함수별 설명 파일 추가
    - 함수 설명 추가 예정.

---
+++ src/taskpane/utils/formulaExplanation.js
@@ -0,0 +1,9 @@
+const FORMULA_EXPLANATION = {
+  IF: "입력한 로직의 참, 거짓 여부에 따라 설정한 반환 값을 반환합니다. ('true', 'false'를 판별할 로직, 'true'일 경우의 반환 값, 'false'일 경우의 반환 값)",
+  AND: "테스트할 로직들이 모두 'true'일 경우, 'true'를 반환합니다.",
+  OR: "테스트할 로직 중, 하나라도 'true'일 경우, 'true'를 반환합니다.",
+  DATEIF:
+    "입력한 날씨 정보를 바탕으로 '필요한 날짜 정보'를 반환합니다. ('시작일', '마지막일', '필요한 날짜 정보')",
+};
+
+export default FORMULA_EXPLANATION;
