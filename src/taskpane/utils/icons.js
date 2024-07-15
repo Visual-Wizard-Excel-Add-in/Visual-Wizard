160a0fe94841a995f38e382dda1a8de5d2f4d658
Pangho04 <dlckdgh741@naver.com>
2024-07-15 23:13:55 +0900

    Add: icons 유틸 파일 생성.
    - EditIcon, SaveIcon을 생성하였습니다.

---
+++ src/taskpane/utils/icons.js
@@ -0,0 +1,12 @@
+import {
+  bundleIcon,
+  Save20Regular,
+  Save20Filled,
+  DocumentEdit20Filled,
+  DocumentEdit20Regular,
+} from "@fluentui/react-icons";
+
+const SaveIcon = bundleIcon(Save20Filled, Save20Regular);
+const EditIcon = bundleIcon(DocumentEdit20Filled, DocumentEdit20Regular);
+
+export { SaveIcon, EditIcon };
