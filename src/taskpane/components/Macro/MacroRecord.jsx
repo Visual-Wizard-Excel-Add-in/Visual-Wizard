e6813044318b33a1d1802b56e0fa87ab6c431d8f
Pangho04 <dlckdgh741@naver.com>
2024-07-16 03:26:05 +0900

    Feat: 매크로 녹화 컴포넌트 정적 구현
    - 매크로 녹화 컴포넌트 정적 구현.
    - 매크로 녹화 시작 및 중지 아이콘 추가.

---
+++ src/taskpane/components/Macro/MacroRecord.jsx
@@ -0,0 +1,43 @@
+import { useStyles } from "../../utils/style";
+import useStore from "../../utils/store";
+import { SaveIcon, EditIcon, RecordStart, RecordStop } from "../../utils/icons";
+import MACRO_PRESETS from "../../utils/MacroPreset";
+import CustomDropdown from "../common/CustomDropdown";
+
+function MacroRecord() {
+  const styles = useStyles();
+  const { isRecording, setIsRecording } = useStore();
+
+  function controlRecoding() {
+    setIsRecording(!isRecording);
+  }
+
+  return (
+    <>
+      <div className="flex items-center justify-between space-x-5">
+        <span>매크로</span>
+        <div className="flex items-center space-x-2">
+          <CustomDropdown options={MACRO_PRESETS} placeholder="매크로" />
+          <button className={styles.buttons} aria-label="save">
+            <EditIcon />
+          </button>
+          <button className={styles.buttons} aria-label="edit">
+            <SaveIcon />
+          </button>
+        </div>
+      </div>
+      <div className="flex items-center justify-between space-x-5">
+        <span className="h-6">매크로 녹화</span>
+        <button
+          onClick={controlRecoding}
+          className={styles.buttons}
+          aria-label="edit"
+        >
+          {isRecording ? <RecordStop /> : <RecordStart color="red" />}
+        </button>
+      </div>
+    </>
+  );
+}
+
+export default MacroRecord;
--- src/taskpane/utils/icons.js
+++ src/taskpane/utils/icons.js
@@ -4,9 +4,15 @@ import {
   Save20Filled,
   DocumentEdit20Filled,
   DocumentEdit20Regular,
+  Record20Regular,
+  Record20Filled,
+  RecordStop20Regular,
+  RecordStop20Filled,
 } from "@fluentui/react-icons";
 
 const SaveIcon = bundleIcon(Save20Filled, Save20Regular);
 const EditIcon = bundleIcon(DocumentEdit20Filled, DocumentEdit20Regular);
+const RecordStart = bundleIcon(Record20Filled, Record20Regular);
+const RecordStop = bundleIcon(RecordStop20Filled, RecordStop20Regular);
 
-export { SaveIcon, EditIcon };
+export { SaveIcon, EditIcon, RecordStart, RecordStop };
