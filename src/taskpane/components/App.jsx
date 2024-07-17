becc144a490727cdbabdfb4b3a6f08515985841a
Pangho04 <dlckdgh741@naver.com>
2024-07-17 20:42:30 +0900

    Feat: 선택한 셀 정보 불러오기 기능 구현

--- src/taskpane/components/App.jsx
+++ src/taskpane/components/App.jsx
@@ -1,3 +1,5 @@
+import { useEffect } from "react";
+
 import useStore from "../utils/store";
 import Header from "./Header";
 import Formula from "./Fomula/Formula";
@@ -6,10 +8,17 @@ import Macro from "./Macro/Macro";
 import Validate from "./Validate/Validate";
 import Share from "./Share/Share";
 import { useStyles } from "../utils/style";
+import { registerSelectionChange, getCellValue } from "../utils/funcUtils";
 
 function App() {
   const styles = useStyles();
-  const { category } = useStore();
+  const {
+    category,
+    setCellArguments,
+    setCellAddress,
+    setCellValue,
+    setCellFormulas,
+  } = useStore();
 
   const categories = {
     Formula: <Formula />,
@@ -19,6 +28,21 @@ function App() {
     Share: <Share />,
   };
   const CurrentCategory = categories[category] || null;
+
+  useEffect(() => {
+    async function registerChange() {
+      await registerSelectionChange(() =>
+        getCellValue(
+          setCellArguments,
+          setCellAddress,
+          setCellValue,
+          setCellFormulas,
+        ),
+      );
+    }
+
+    registerChange();
+  }, []);
 
   return (
     <div className={styles.root}>
