d189382bdfab6496416902573552bad674a30cce
Pangho04 <dlckdgh741@naver.com>
2024-07-18 21:50:53 +0900

    Feat: 셀 정보 및 스타일 정보 State 추가.

--- src/taskpane/utils/store.js
+++ src/taskpane/utils/store.js
@@ -7,6 +7,20 @@ const useStore = create((set) => ({
   setOpenTab: (openedTabs) => set({ openTab: openedTabs }),
   isRecording: false,
   setIsRecording: (recordState) => set({ isRecording: recordState }),
+  isCellHighlighting: false,
+  setIsCellHighlighting: () =>
+    set((state) => ({ isCellHighlighting: !state.isCellHighlighting })),
+  cellValue: "",
+  setCellValue: (selectedCellValue) => set({ cellValue: selectedCellValue }),
+  cellAddress: "",
+  setCellAddress: (selectedCellAddress) =>
+    set({ cellAddress: selectedCellAddress }),
+  cellFormulas: [],
+  setCellFormulas: (selectedCellFormulas) =>
+    set({ cellFormulas: selectedCellFormulas }),
+  cellArguments: [],
+  setCellArguments: (selectedCellArguments) =>
+    set({ cellArguments: selectedCellArguments }),
 }));
 
 export default useStore;
