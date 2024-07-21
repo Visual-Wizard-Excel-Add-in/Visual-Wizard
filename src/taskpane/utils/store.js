305fbddc7a570cb454aaed9f5ab22229e3f318b5
Pangho04 <dlckdgh741@naver.com>
2024-07-21 19:31:06 +0900

    Feat: 상태 추가 및 수정
    - 추가:
    'sheeId',
    'formulaSteps',
    'cellFunctions'.

--- src/taskpane/utils/store.js
+++ src/taskpane/utils/store.js
@@ -3,21 +3,39 @@ import { create } from "zustand";
 const useStore = create((set) => ({
   category: "Formula",
   setCategory: (selectedCategory) => set({ category: selectedCategory }),
+
   openTab: [],
   setOpenTab: (openedTabs) => set({ openTab: openedTabs }),
+
   isRecording: false,
   setIsRecording: (recordState) => set({ isRecording: recordState }),
+
   isCellHighlighting: false,
   setIsCellHighlighting: () =>
     set((state) => ({ isCellHighlighting: !state.isCellHighlighting })),
+
+  sheetId: "",
+  setSheetId: (selectSheet) => set({ sheetName: selectSheet }),
+
   cellValue: "",
   setCellValue: (selectedCellValue) => set({ cellValue: selectedCellValue }),
+
   cellAddress: "",
   setCellAddress: (selectedCellAddress) =>
     set({ cellAddress: selectedCellAddress }),
-  cellFormulas: [],
-  setCellFormulas: (selectedCellFormulas) =>
-    set({ cellFormulas: selectedCellFormulas }),
+
+  cellFormula: "",
+  setCellFormula: (selectedCellFormula) =>
+    set({ cellFormula: selectedCellFormula }),
+
+  formulaSteps: [],
+  setFormulaSteps: (currentFormulaSteps) =>
+    set({ formulaSteps: currentFormulaSteps }),
+
+  cellFunctions: [],
+  setCellFunctions: (selectedCellFunctions) =>
+    set({ cellFunctions: selectedCellFunctions }),
+
   cellArguments: [],
   setCellArguments: (selectedCellArguments) =>
     set({ cellArguments: selectedCellArguments }),
