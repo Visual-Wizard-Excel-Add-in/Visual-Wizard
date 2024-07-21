f629dad41fe77eba49f2a687ffecbac5161a24da
Pangho04 <dlckdgh741@naver.com>
2024-07-21 20:44:08 +0900

    Refactor: 범위 셀 참조 시, 범위 출력

--- src/taskpane/components/Fomula/FormulaAttribute.jsx
+++ src/taskpane/components/Fomula/FormulaAttribute.jsx
@@ -1,10 +1,11 @@
 import { Switch } from "@fluentui/react-components";
 
 import useStore from "../../utils/store";
-import { highlightingCell } from "../../utils/funcUtils";
+import { highlightingCell } from "../../utils/cellStyleFunc";
+import { groupCellsIntoRanges } from "../../utils/cellFormulaFunc";
 
 function FormulaAttribute() {
-  const { cellArguments, cellAddress, cellValue, cellFormulas } = useStore();
+  const { cellArguments, cellAddress, cellValue, cellFunctions } = useStore();
 
   async function handleHighlighting() {
     useStore.setState((state) => {
@@ -18,32 +19,50 @@ function FormulaAttribute() {
     });
   }
 
+  const groupedCellArguments = groupCellsIntoRanges(
+    cellArguments.map((arg) => arg.split("(")[0]),
+  );
+
+  const formattedCellArguments = groupedCellArguments
+    .map((arg) => {
+      const matchingArg = cellArguments.find((ca) => ca.startsWith(arg));
+      if (matchingArg && !arg.includes(":")) {
+        return `${arg}(${matchingArg.split("(")[1]}`;
+      }
+      return arg;
+    })
+    .join(", ");
+
+  const resultCellAddress = cellAddress.split("!")[1];
+
   return (
     <div>
       <Switch
         label="현재 셀 강조하기"
         onChange={handleHighlighting}
-        disabled={cellFormulas.length === 0}
+        disabled={cellFunctions.length === 0}
       />
       <div>
         <p className="font-bold">
           <img
-            src="assets/highlightArgumentCell.png"
+            src="src/taskpane/assets/highlightArgumentCell.png"
             alt="highlightArgCells"
             className="inline"
           />
           &nbsp;인수:&nbsp;
-          <span className="font-normal">{cellArguments.join(", ")}</span>
+          <span className="font-normal">{formattedCellArguments}</span>
         </p>
         <p className="mb-2 font-bold">
           <img
-            src="assets/highlightResultCell.png"
+            src="src/taskpane/assets/highlightResultCell.png"
             alt="highlightResultCells"
             className="inline"
           />
           &nbsp;결과:&nbsp;
           <span className="font-normal">
-            {cellFormulas.length !== 0 ? `${cellAddress}(${cellValue})` : null}
+            {cellFunctions.length !== 0
+              ? `${resultCellAddress}(${cellValue})`
+              : null}
           </span>
         </p>
       </div>
