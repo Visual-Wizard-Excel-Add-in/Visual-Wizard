20d703adbb5c9c02c5dcbbbab661d7c66f3ef31c
Pangho04 <dlckdgh741@naver.com>
2024-07-21 21:12:36 +0900

    Feat: 수식 카테고리 순서 기능 구현
    - 선택한 셀의 수식 내의 함수를 순서대로 리스트업 합니다.
    - 함수명 클릭시 해당 함수의 참조, 수식, 조건 등을 볼 수 있습니다.

--- src/taskpane/components/Fomula/FormulaOrder.jsx
+++ src/taskpane/components/Fomula/FormulaOrder.jsx
@@ -1,30 +1,59 @@
+import { useEffect } from "react";
 import { Button } from "@fluentui/react-components";
-import useStore from "../../utils/store";
 
+import { parseFormulaSteps } from "../../utils/cellFormulaFunc";
+import useStore from "../../utils/store";
 import CustomPopover from "../common/CustomPopover";
+import FormulaOrderDescription from "./FormulaOrderDescription";
 
 function FormulaOrder() {
+  const { cellFormula, formulaSteps, setFormulaSteps } = useStore();
+
+  useEffect(() => {
+    async function fetchFormulaSteps() {
+      if (cellFormula) {
+        try {
+          const result = await parseFormulaSteps();
+          useStore.getState().setFormulaSteps(result);
+        } catch (error) {
+          setFormulaSteps([]);
+        }
+      } else {
+        useStore.getState().setFormulaSteps([]);
+      }
+    }
+
+    fetchFormulaSteps();
+  }, [cellFormula]);
+
   function trigger(text) {
     return <Button>{text}</Button>;
   }
-  const { cellFormulas } = useStore();
+
+  function renderFormulaSteps(steps) {
+    return steps.map((step, index) => {
+      const func = step.functionName;
+      const description = <FormulaOrderDescription step={step} />;
+      return (
+        <div key={`${step.address}-${func}}`}>
+          <span>{index + 1}. </span>
+          <CustomPopover
+            position="after"
+            triggerContents={trigger(func)}
+            PopoverContents={description}
+          />
+        </div>
+      );
+    });
+  }
 
   return (
     <div>
-      {cellFormulas.map((formula, index) => {
-        const func = Object.keys(formula)[0];
-        const description = formula[func];
-        return (
-          <div key={{ func } + { index }} className="mb-2">
-            <span>{index + 1}. </span>
-            <CustomPopover
-              position="after"
-              triggerContents={trigger(func)}
-              PopoverContents={description}
-            />
-          </div>
-        );
-      })}
+      {formulaSteps && formulaSteps.length !== 0 ? (
+        renderFormulaSteps(formulaSteps)
+      ) : (
+        <div>수식이 입력된 셀을 선택해주세요.</div>
+      )}
     </div>
   );
 }
---
+++ src/taskpane/components/Fomula/FormulaOrderDescription.jsx
@@ -0,0 +1,163 @@
+import { useState } from "react";
+import { useStyles } from "../../utils/style";
+
+function FormulaOrderDescription({ step }) {
+  const [isExpandedCondition, setIsExpandedCondition] = useState(false);
+  const [isExpandedTrueValue, setIsExpandedTrueValue] = useState(false);
+  const [isExpandedFalseValue, setIsExpandedFalseValue] = useState(false);
+  const [isExpandedFormula, setIsExpandedFormula] = useState(false);
+
+  const styles = useStyles();
+
+  function truncateText(text, maxLength, isExpanded) {
+    if (text.length > maxLength && !isExpanded) {
+      return `${text.substring(0, maxLength)}...`;
+    }
+    return text;
+  }
+
+  const {
+    functionName,
+    condition: stepCondition,
+    trueValue,
+    falseValue,
+    criteriaRange,
+    criteria,
+    formula,
+  } = step;
+
+  const renderCondition = (condition, maxLength, isExpanded, setIsExpanded) => (
+    <div>
+      <strong>조건: {truncateText(condition, maxLength, isExpanded)}</strong>
+      {condition.length > maxLength && (
+        <div>
+          <button
+            className={styles.blurText}
+            onClick={() => setIsExpanded(!isExpanded)}
+          >
+            {isExpanded ? "[간략히]" : "[더보기]"}
+          </button>
+        </div>
+      )}
+    </div>
+  );
+
+  const renderCriteriaRange = (range, maxLength, isExpanded, setIsExpanded) => (
+    <div>
+      <strong>조건 범위: {truncateText(range, maxLength, isExpanded)}</strong>
+      {range.length > maxLength && (
+        <div>
+          <button
+            className={styles.blurText}
+            onClick={() => setIsExpanded(!isExpanded)}
+          >
+            {isExpanded ? "[간략히]" : "[더보기]"}
+          </button>
+        </div>
+      )}
+    </div>
+  );
+
+  return (
+    <div className="break-all">
+      <strong>참조 셀: {step.address}</strong>
+      <br />
+      {[
+        "IF",
+        "IFS",
+        "SUMIF",
+        "COUNTIF",
+        "AVERAGEIF",
+        "IFERROR",
+        "IFNA",
+        "SWITCH",
+        "CHOOSE",
+      ].includes(functionName) && (
+        <div>
+          {stepCondition &&
+            renderCondition(
+              stepCondition,
+              35,
+              isExpandedCondition,
+              setIsExpandedCondition,
+            )}
+          {trueValue && (
+            <div>
+              <strong>
+                참일 때: {truncateText(trueValue, 35, isExpandedTrueValue)}
+              </strong>
+              {trueValue.length > 35 && (
+                <div>
+                  <button
+                    className={styles.blurText}
+                    onClick={() => setIsExpandedTrueValue(!isExpandedTrueValue)}
+                  >
+                    {isExpandedTrueValue ? "[간략히]" : "[더보기]"}
+                  </button>
+                </div>
+              )}
+            </div>
+          )}
+          {falseValue && (
+            <div>
+              <strong>
+                거짓일 때: {truncateText(falseValue, 35, isExpandedFalseValue)}
+              </strong>
+              {falseValue.length > 35 && (
+                <div>
+                  <button
+                    className={styles.blurText}
+                    onClick={() =>
+                      setIsExpandedFalseValue(!isExpandedFalseValue)
+                    }
+                  >
+                    {isExpandedFalseValue ? "[간략히]" : "[더보기]"}
+                  </button>
+                </div>
+              )}
+            </div>
+          )}
+        </div>
+      )}
+      {[
+        "SUMIF",
+        "SUMIFS",
+        "COUNTIF",
+        "COUNTIFS",
+        "AVERAGEIF",
+        "AVERAGEIFS",
+      ].includes(functionName) && (
+        <div>
+          {criteriaRange &&
+            renderCriteriaRange(
+              criteriaRange,
+              35,
+              isExpandedCondition,
+              setIsExpandedCondition,
+            )}
+          {criteria &&
+            renderCondition(
+              criteria,
+              35,
+              isExpandedCondition,
+              setIsExpandedCondition,
+            )}
+        </div>
+      )}
+      <br />
+      <strong>식: {truncateText(formula, 35, isExpandedFormula)}</strong>
+      {formula.length > 35 && (
+        <div>
+          <button
+            className={styles.blurText}
+            onClick={() => setIsExpandedFormula(!isExpandedFormula)}
+          >
+            {isExpandedFormula ? "[간략히]" : "[더보기]"}
+          </button>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default FormulaOrderDescription;
