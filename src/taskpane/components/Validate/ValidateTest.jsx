7ae8feea383d8a2da6b8c5026e10ebf0fe51433a
Pangho04 <dlckdgh741@naver.com>
2024-07-25 05:14:32 +0900

    Feat: 유효성 카테고리 유효성 검사 기능 구현
    - 에러 셀 도출 기능 구현.
    - 가장 바깥 셀 찾기 기능 구현.

--- src/taskpane/components/Validate/ValidateTest.jsx
+++ src/taskpane/components/Validate/ValidateTest.jsx
@@ -1,11 +1,36 @@
+import { useState, useEffect } from "react";
 import { Switch } from "@fluentui/react-components";
 
+import { getLastCellAddress } from "../../utils/cellCommonUtils";
+import { detectErrorCell } from "../../utils/cellStyleFunc";
+
 function ValidateTest() {
-  const lastCell = "D8";
+  const [isError, setIsError] = useState(true);
+  const [lastCell, setLastCell] = useState("");
+
+  useEffect(() => {
+    const fetchLastCellAddress = async () => {
+      const address = await getLastCellAddress();
+
+      setLastCell(address);
+    };
+
+    fetchLastCellAddress();
+  }, [lastCell]);
+
+  function highlightError() {
+    detectErrorCell(isError);
+    setIsError(!isError);
+  }
 
   return (
     <div>
-      <Switch label="에러 셀 검사" onChange={() => {}} />
+      <Switch
+        label="에러 셀 검사"
+        onChange={() => {
+          highlightError();
+        }}
+      />
       <p>
         사용중인 마지막 셀 영역:&nbsp;
         <span className="font-bold">{lastCell}</span>
