bb1cc5067810ea29b62673e497182a4a3da066df
Pangho04 <dlckdgh741@naver.com>
2024-07-29 05:23:41 +0900

    Feat: 매크로 프리셋 대응 수정
    - 옵션 리스트 그룹화 기능 추가.

--- src/taskpane/components/common/CustomDropdown.jsx
+++ src/taskpane/components/common/CustomDropdown.jsx
@@ -1,26 +1,67 @@
-import { Dropdown, Option, useId } from "@fluentui/react-components";
+import {
+  Dropdown,
+  Option,
+  OptionGroup,
+  useId,
+} from "@fluentui/react-components";
+import { useStyles } from "../../utils/style";
 
-function CustomDropdown({ handleValue, options, placeholder, selectedValue }) {
-  const selectId = useId();
+function CustomDropdown({
+  handleValue,
+  options,
+  placeholder,
+  selectedValue = null,
+}) {
+  const styles = useStyles();
 
   function handleChange(event, value) {
     event.stopPropagation();
-
     handleValue(value.optionText);
   }
 
+  const groupedOptions = options.reduce((acc, option) => {
+    const label = option.label || "";
+
+    if (!acc[label]) {
+      acc[label] = [];
+    }
+
+    acc[label].push(option);
+
+    return acc;
+  }, {});
+
   return (
     <Dropdown
-      id={selectId}
+      positioning="below"
       placeholder={placeholder}
+      button={
+        <span className={styles.optionBox}>{selectedValue || placeholder}</span>
+      }
       onOptionSelect={handleChange}
       value={selectedValue}
     >
-      {options.map((option) => (
-        <Option key={option.value} value={option.value} className="!w-32">
-          {option.name}
-        </Option>
-      ))}
+      {Object.entries(groupedOptions).map(([label, categorizedOptions]) =>
+        label ? (
+          <OptionGroup key={`group-${label}`} label={label}>
+            {categorizedOptions.map((option) => (
+              <Option key={`option-${option.value}`} value={option.value}>
+                {option.name}
+              </Option>
+            ))}
+          </OptionGroup>
+        ) : (
+          categorizedOptions.map((option) => (
+            <Option
+              key={`without group-${option.value}`}
+              value={option.value}
+              className="!w-fit"
+            >
+              {option.name}
+            </Option>
+          ))
+        ),
+      )}
     </Dropdown>
   );
 }
