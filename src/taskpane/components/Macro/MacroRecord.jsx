efee4189b4eb88ec587f439b8cf91f738149aaa4
Pangho04 <dlckdgh741@naver.com>
2024-07-29 05:29:58 +0900

    Feat: 매크로 녹화 기능 구현.

--- src/taskpane/components/Macro/MacroRecord.jsx
+++ src/taskpane/components/Macro/MacroRecord.jsx
@@ -1,3 +1,6 @@
+import { useState, useEffect } from "react";
+import { Button } from "@fluentui/react-components";
+
 import { useStyles } from "../../utils/style";
 import useStore from "../../utils/store";
 import {
@@ -7,37 +10,145 @@ import {
   RecordStop,
   PlusIcon,
 } from "../../utils/icons";
-import MACRO_PRESETS from "../../Presets/MacroPreset";
 import CustomDropdown from "../common/CustomDropdown";
+import {
+  addPreset,
+  deletePreset,
+  updateState,
+} from "../../utils/cellCommonUtils";
+import { manageRecording, macroPlay } from "../../utils/macroFuncs";
 
 function MacroRecord() {
+  const [macroPresets, setMacroPresets] = useState([]);
   const styles = useStyles();
-  const { isRecording, setIsRecording } = useStore();
+  const {
+    isRecording,
+    setIsRecording,
+    selectMacroPreset,
+    setSelectMacroPreset,
+  } = useStore();
 
-  function controlRecoding() {
+  useEffect(() => {
+    async function fetchPresets() {
+      const savedPresets = await loadPresets();
+
+      setMacroPresets(Object.keys(savedPresets));
+    }
+
+    fetchPresets();
+  }, []);
+
+  async function newPreset() {
+    let lastPresetNum = 0;
+
+    if (macroPresets.length > 0) {
+      lastPresetNum = Number(
+        macroPresets[macroPresets.length - 1].split("로")[1],
+      );
+    }
+
+    if (macroPresets.includes("매크로1")) {
+      await addPreset("allMacroPresets", `매크로${lastPresetNum + 1}`);
+    } else {
+      await addPreset("allMacroPresets", "매크로1");
+    }
+
+    const savedPresets = await loadPresets();
+    const sortedPresets = Object.keys(savedPresets).sort((a, b) =>
+      a.localeCompare(b),
+    );
+
+    setMacroPresets(sortedPresets);
+  }
+
+  function controlMacroRecording() {
+    if (setSelectMacroPreset === "") {
+      updateState("setMessageList", {
+        type: "warning",
+        title: "접근 오류:",
+        body: "프리셋을 선택해주세요.",
+      });
+
+      return;
+    }
+
+    manageRecording(!isRecording, selectMacroPreset);
     setIsRecording(!isRecording);
+  }
+
+  async function loadPresets() {
+    let presets = await OfficeRuntime.storage.getItem("allMacroPresets");
+
+    if (!presets) {
+      presets = {};
+    } else {
+      presets = JSON.parse(presets);
+    }
+
+    return presets;
+  }
+
+  async function handleDeletePreset() {
+    if (!setSelectMacroPreset) {
+      return;
+    }
+
+    await deletePreset("allMacroPresets", selectMacroPreset);
+
+    const savedPresets = await loadPresets();
+
+    setSelectMacroPreset("");
+    setMacroPresets(Object.keys(savedPresets));
   }
 
   return (
     <>
       <div className="flex items-center justify-between space-x-5">
-        <div className="flex items-center space-x-2">
-          <button className={styles.buttons} aria-label="plus">
+        <div className="flex items-center w-8/12 space-x-2">
+          <button
+            onClick={newPreset}
+            className={styles.buttons}
+            aria-label="plus"
+          >
             <PlusIcon />
           </button>
-          <CustomDropdown options={MACRO_PRESETS} placeholder="매크로" />
-          <button className={styles.buttons} aria-label="delete">
+          <CustomDropdown
+            handleValue={(value) => setSelectMacroPreset(value)}
+            options={macroPresets.map((preset) => ({
+              name: preset,
+              value: preset,
+            }))}
+            placeholder="매크로"
+            selectedValue={selectMacroPreset}
+          />
+          <button
+            onClick={handleDeletePreset}
+            className={styles.buttons}
+            aria-label="delete"
+          >
             <DeleteIcon />
           </button>
-          <button className={styles.buttons} aria-label="save">
+          <button
+            onClick={() => {}}
+            className={styles.buttons}
+            aria-label="save"
+          >
             <SaveIcon />
           </button>
         </div>
+        <Button
+          as="button"
+          className="self-center"
+          onClick={() => macroPlay(selectMacroPreset)}
+          size="small"
+        >
+          실행
+        </Button>
       </div>
       <div className="flex items-center justify-between space-x-5">
         <span className="h-6">매크로 녹화</span>
         <button
-          onClick={controlRecoding}
+          onClick={controlMacroRecording}
           className={styles.buttons}
           aria-label="record"
         >
