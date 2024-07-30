7e54595e5423d8d66e4b62a64368d5e067ee393b
Pangho04 <dlckdgh741@naver.com>
2024-07-31 07:42:36 +0900

    Feat: 추출 기능 컴포넌트 구현

---
+++ src/taskpane/components/Share/Extraction.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import { Button, Divider } from "@fluentui/react-components";
+import CustomDropdown from "../common/CustomDropdown";
+import executeFunction from "../../utils/extractFileFunc";
+
+function Extraction() {
+  const [dataLocation, setDataLocation] = useState("선택 영역");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const dataLocationOptions = [
+    { name: "선택 영역", value: "selectRange" },
+    { name: "현재 시트", value: "currentSheet" },
+  ];
+
+  const handleSaveData = async () => {
+    setIsLoading(true);
+    await executeFunction(dataLocation);
+    setIsLoading(false);
+  };
+
+  return (
+    <>
+      <div className="flex items-center justify-between space-x-5">
+        <p>저장할 자료 위치</p>
+        <CustomDropdown
+          options={dataLocationOptions}
+          handleValue={(value) => setDataLocation(value)}
+          selectedValue={dataLocation}
+          placeholder="데이터 선택"
+        />
+      </div>
+      <Divider className="my-2" appearance="strong" />
+      <div className="flex justify-center">
+        <Button onClick={handleSaveData}>저장</Button>
+        {isLoading && (
+          <div>
+            <span>저장 중...</span>
+          </div>
+        )}
+      </div>
+    </>
+  );
+}
+
+export default Extraction;
