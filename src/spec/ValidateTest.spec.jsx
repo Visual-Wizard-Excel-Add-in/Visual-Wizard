f545b58807acc9814a752f2eb0c2f729bab3a6e7
Pangho04 <dlckdgh741@naver.com>
2024-08-16 15:03:11 +0900

    Test: ValidateTest 컴포넌트 테스트 코드 작성
    - 컴포넌트가 렌더링 될 때, fetchLastcellAddress가 호출되는지 확인합니다.
    - '에러 셀 검사' 버튼을 토글할 경우, highlightError 함수가 호출되는지 확인합니다.

---
+++ src/spec/ValidateTest.spec.jsx
@@ -0,0 +1,67 @@
+import {
+  render,
+  screen,
+  fireEvent,
+  waitFor,
+  act,
+} from "@testing-library/react";
+
+import ValidateTest from "../taskpane/components/Validate/ValidateTest";
+import { getLastCellAddress } from "../taskpane/utils/cellCommonUtils";
+import { detectErrorCell } from "../taskpane/utils/cellStyleFunc";
+
+global.Excel = {
+  run: vi.fn().mockImplementation(async (context) => {
+    if (typeof context === "function") {
+      const fakeContext = {
+        workbook: {
+          worksheets: {
+            onSelectionChanged: {
+              add: vi.fn(),
+            },
+            getActiveWorksheet: () => ({
+              load: vi.fn(),
+              id: "1",
+            }),
+          },
+        },
+        sync: vi.fn(),
+      };
+      return await context(fakeContext);
+    }
+    return Promise.resolve();
+  }),
+};
+
+vi.mock("../taskpane/utils/cellStyleFunc");
+vi.mock("../taskpane/utils/cellCommonUtils");
+
+describe("ValidateTest", () => {
+  it("When rendering the component, fetchLastcellAddress functions should called", async () => {
+    const fetchLastCellAddress = vi.fn().mockResolvedValue("A1");
+    getLastCellAddress.mockImplementation(fetchLastCellAddress);
+
+    render(<ValidateTest />);
+
+    await waitFor(() => {
+      expect(fetchLastCellAddress).toHaveBeenCalled();
+    });
+
+    const container = screen.getByText("사용중인 마지막 셀 영역:").closest("p");
+    expect(container).toBeInTheDocument();
+
+    expect(container).toHaveTextContent("사용중인 마지막 셀 영역: A1");
+  });
+
+  it("When toggle the '에러 셀 검사' button, highlightError function should called", async () => {
+    render(<ValidateTest />);
+
+    const highlightButton = screen.getByLabelText("에러 셀 검사");
+
+    await act(async () => {
+      fireEvent.click(highlightButton);
+    });
+
+    expect(detectErrorCell).toHaveBeenCalledWith(true);
+  });
+});
