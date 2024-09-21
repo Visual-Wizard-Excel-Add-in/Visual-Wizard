1463d630c83c236c95d5a0f7c1841cf18cd11fd9
Pangho04 <dlckdgh741@naver.com>
2024-09-21 23:47:46 +0900

    Feat: 전역 interface 선언 추가

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,1 +1,22 @@
 declare const OfficeRuntime: typeof import("office-runtime");
+
+interface GraphNodeType {
+  address: string;
+  condition?: string;
+  dependencies?: GraphNodeType[];
+  falseValue?: string;
+  formula: string;
+  functionName?: string;
+  trueValue?: string;
+  values?: string[];
+  criteriaRange?: string;
+  criteria?: string | string[];
+  sumRange?: string;
+  conditions?: string[];
+  criteriaRanges?: string[];
+}
+
+interface GraphType {
+  data: GraphNodeType;
+  dependencies: Set<GraphType>;
+}
