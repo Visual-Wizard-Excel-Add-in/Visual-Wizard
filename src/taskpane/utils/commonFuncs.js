f39e83b1a71e9c115363986f234ac72281d54df5
Pangho04 <dlckdgh741@naver.com>
2024-10-04 15:50:22 +0900

    Refactor: popUpMessage() Switch문 함수 분리

--- src/taskpane/utils/commonFuncs.js
+++ src/taskpane/utils/commonFuncs.js
@@ -284,59 +284,53 @@ async function deletePreset(presetCategory, presetName) {
 }
 
 function popUpMessage(purpose = null, option = "") {
-  let message = null;
+  updateState("setMessageList", getMessage());
 
-  switch (purpose) {
-    case "loadFail":
-      message = {
-        type: "warning",
-        title: "Load Failed:",
-        body: `데이터를 불러오는데 실패했습니다.\n${option}`,
-      };
-      break;
+  function getMessage() {
+    switch (purpose) {
+      case "loadFail":
+        return {
+          type: "warning",
+          title: "Load Failed:",
+          body: `데이터를 불러오는데 실패했습니다.\n${option}`,
+        };
 
-    case "saveFail":
-      message = {
-        type: "warning",
-        title: "Save Failed:",
-        body: `데이터를 저장하는데 실패했습니다.\n${option}`,
-      };
-      break;
+      case "saveFail":
+        return {
+          type: "warning",
+          title: "Save Failed:",
+          body: `데이터를 저장하는데 실패했습니다.\n${option}`,
+        };
 
-    case "workFail":
-      message = {
-        type: "warning",
-        title: "Work Failed",
-        body: `실행에 실패했습니다.\n${option}`,
-      };
-      break;
+      case "workFail":
+        return {
+          type: "warning",
+          title: "Work Failed",
+          body: `실행에 실패했습니다.\n${option}`,
+        };
 
-    case "saveSuccess":
-      message = {
-        type: "success",
-        title: "Saved",
-        body: `데이터를 저장했습니다.\n${option}`,
-      };
-      break;
+      case "saveSuccess":
+        return {
+          type: "success",
+          title: "Saved",
+          body: `데이터를 저장했습니다.\n${option}`,
+        };
 
-    case "loadSuccess":
-      message = {
-        type: "success",
-        title: "Loaded",
-        body: `데이터를 불러왔습니다.\n${option}`,
-      };
-      break;
+      case "loadSuccess":
+        return {
+          type: "success",
+          title: "Loaded",
+          body: `데이터를 불러왔습니다.\n${option}`,
+        };
 
-    default:
-      message = {
-        type: "warning",
-        title: "Undefiend Error:",
-        body: `예상하지 못한 에러가 발생했습니다.\n${option}`,
-      };
-      break;
+      default:
+        return {
+          type: "warning",
+          title: "Undefiend Error:",
+          body: `예상하지 못한 에러가 발생했습니다.\n${option}`,
+        };
+    }
   }
-
-  updateState("setMessageList", message);
 }
 
 export {
