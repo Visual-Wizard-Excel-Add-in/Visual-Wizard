4e6a506cd00aa938c46465cf16f1f48d08f653da
Pangho04 <dlckdgh741@naver.com>
2024-10-12 14:19:34 +0900

    Refactor: 불필요한 요소, 함수 삭제
    - <OptionGroup> 미사용

--- src/taskpane/components/common/CustomDropdown.jsx
+++ src/taskpane/components/common/CustomDropdown.jsx
@@ -1,4 +1,4 @@
-import { Dropdown, Option, OptionGroup } from "@fluentui/react-components";
+import { Dropdown, Option } from "@fluentui/react-components";
 import { useStyles } from "../../utils/style";
 
 function CustomDropdown({
@@ -14,18 +14,6 @@ function CustomDropdown({
     handleValue(value.optionText);
   };
 
-  const groupedOptions = options.reduce((acc, option) => {
-    const label = option.label || "";
-
-    if (!acc[label]) {
-      acc[label] = [];
-    }
-
-    acc[label].push(option);
-
-    return acc;
-  }, {});
-
   return (
     <Dropdown
       positioning="below"
@@ -36,27 +24,15 @@ function CustomDropdown({
       onOptionSelect={handleChange}
       value={selectedValue}
     >
-      {Object.entries(groupedOptions).map(([label, categorizedOptions]) =>
-        label ? (
-          <OptionGroup key={`group-${label}`} label={label}>
-            {categorizedOptions.map((option) => (
-              <Option key={`option-${option.value}`} value={option.value}>
-                {option.name}
-              </Option>
-            ))}
-          </OptionGroup>
-        ) : (
-          categorizedOptions.map((option) => (
-            <Option
-              key={`without group-${option.value}`}
-              value={option.value}
-              className="!w-fit"
-            >
-              {option.name}
-            </Option>
-          ))
-        ),
-      )}
+      {options.map((option) => (
+        <Option
+          key={`without group-${option.value}`}
+          value={option.value}
+          className="!w-fit"
+        >
+          {option.name}
+        </Option>
+      ))}
     </Dropdown>
   );
 }
