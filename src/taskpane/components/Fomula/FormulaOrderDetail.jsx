78bf5820d918f429c836b5e8db470192536e19e6
Pangho04 <dlckdgh741@naver.com>
2024-10-15 00:58:56 +0900

    Refactor: 조건부 함수 리스트 상수 추출

---
+++ src/taskpane/components/Fomula/FormulaOrderDetail.jsx
@@ -0,0 +1,4 @@
+import CONDITION_FUNCTIONS_LIST from "../../constants/conditionFuncsListConstans";
+
+      {CONDITION_FUNCTIONS_LIST.single.includes(functionName) && (
+      {CONDITION_FUNCTIONS_LIST.serveral.includes(functionName) && (
---
+++ src/taskpane/constants/conditionFuncsListConstans.js
@@ -0,0 +1,23 @@
+const CONDITION_FUNCTIONS_LIST = {
+  single: [
+    "IF",
+    "IFS",
+    "SUMIF",
+    "COUNTIF",
+    "AVERAGEIF",
+    "IFERROR",
+    "IFNA",
+    "SWITCH",
+    "CHOOSE",
+  ],
+  serveral: [
+    "SUMIF",
+    "SUMIFS",
+    "COUNTIF",
+    "COUNTIFS",
+    "AVERAGEIF",
+    "AVERAGEIFS",
+  ],
+};
+
+export default CONDITION_FUNCTIONS_LIST;
