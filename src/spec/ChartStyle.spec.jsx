0bcb11110b33a83e93e1bc7e54c4284e95fda5ed
Pangho04 <dlckdgh741@naver.com>
2024-11-08 03:21:57 +0900

    Test: <ChartStyle>, Test 수정 및 보충
     - 불필요한 데이터(Mock, Import 등)을 제거하고, 테스트를 수정했습니다.

--- src/spec/ChartStyle.spec.jsx
+++ src/spec/ChartStyle.spec.jsx
@@ -1,113 +1,169 @@
-import {
-  render,
-  screen,
-  fireEvent,
-  waitFor,
-  act,
-} from "@testing-library/react";
+import { render, screen, waitFor, act } from "@testing-library/react";
+import { userEvent } from "@testing-library/user-event";
+import { OfficeMockObject } from "office-addin-mock";
 
 import ChartStyle from "../taskpane/components/Style/ChartStyle";
-import { addPreset, deletePreset } from "../taskpane/utils/commonFuncs";
-import {
-  copyChartStyle,
-  pasteChartStyle,
-} from "../taskpane/utils/chartStyleFuncs";
+import { mockChartStyle, savedChartStyle } from "./test-constants";
 
-vi.mock("../taskpane/utils/commonFuncs");
-vi.mock("../taskpane/utils/cellStyleFuncs");
-vi.mock("../taskpane/utils/chartStyleFuncs");
-
-global.OfficeRuntime = {
+const OfficeRuntimeMock = {
   storage: {
-    getItem: vi.fn().mockResolvedValue("{}"),
-    setItem: vi.fn().mockResolvedValue(),
+    _storage: {},
+    getItem: vi.fn(async (key) =>
+      JSON.stringify(OfficeRuntimeMock.storage._storage[key] || {}),
+    ),
+    setItem: vi.fn(async (key, value) => {
+      OfficeRuntimeMock.storage._storage[key] = JSON.parse(value);
+    }),
   },
 };
 
+const mockData = {
+  context: {
+    workbook: {
+      worksheets: {
+        add: vi.fn(() => mockData.context.workbook.worksheet),
+        getItemOrNullObject: vi.fn((sheetName) => {
+          if ("StyleSheet" === sheetName) {
+            return {
+              ...mockData.context.workbook.worksheet,
+              isNullObject: false,
+            };
+          }
+
+          return { isNullObject: true };
+        }),
+      },
+      worksheet: {
+        delete: vi.fn(),
+        setCellRange: vi.fn(),
+        getRange: vi.fn(() => mockData.context.workbook.range),
+      },
+      range: {
+        address: "Sheet!A1",
+        format: {},
+        getCellProperties: vi.fn(function () {
+          return {
+            value: mockStyle,
+          };
+        }),
+        setCellProperties: vi.fn(
+          (source) => (mockData.context.workbook.range.format = source),
+        ),
+        copyFrom: vi.fn(),
+      },
+      chartType: {
+        ...mockChartStyle,
+      },
+      // _chartType: {
+      //   load: vi.fn(),
+      //   foramt: {
+      //     fill: {
+      //       setSolidColor: vi.fn((color) => (this.format.fill.color = color)),
+      //       color: "",
+      //     },
+      //   },
+      //   plotArea: {
+      //     format: {
+      //       fill: {
+      //         color: "",
+      //         setSolidColor: vi.fn(
+      //           (color) =>
+      //             (this.format.fill.plotArea.foramt.fill.color = color),
+      //         ),
+      //       },
+      //     },
+      //   },
+      //   legend: {
+      //     format: {
+      //       fill: {
+      //         color: "",
+      //         setSolidColor: vi.fn(
+      //           (color) =>
+      //             (this.format.fill.plotArea.legend.fill.color = color),
+      //         ),
+      //       },
+      //     },
+      //   },
+      //   series: {
+      //     items: [
+      //       {
+      //         load: vi.fn(),
+      //         format: {
+      //           fill: {
+      //             color: "",
+      //             setSolidColor: vi.fn(
+      //               (color) =>
+      //                 (this.format.fill.plotArea.foramt.fill.color = color),
+      //             ),
+      //           },
+      //         },
+      //       },
+      //     ],
+      //   },
+      // },
+      getActiveChart: vi.fn(function () {
+        // if ((await loadStorage()) === savedChartStyle) {
+        //   return this._chartType;
+        // }
+
+        return this.chartType;
+      }),
+      getSelectedRange: vi.fn(function () {
+        return this.range;
+      }),
+    },
+  },
+  run: vi.fn(async function (callback) {
+    await callback(this.context);
+  }),
+};
+
+async function loadStorage() {
+  return JSON.parse(await OfficeRuntime.storage.getItem("chartStylePresets"));
+}
+
+vi.stubGlobal("Excel", new OfficeMockObject(mockData));
+vi.stubGlobal("OfficeRuntime", OfficeRuntimeMock);
+
 describe("ChartStyle", () => {
-  beforeEach(() => {
-    vi.clearAllMocks();
-  });
-
-  it("should create a new chart style preset", async () => {
-    addPreset.mockResolvedValue();
-
+  it("프리셋 '+'버튼 클릭 시, 프리셋이 추가되어야 한다.", async () => {
     render(<ChartStyle />);
 
-    await act(async () => {
-      fireEvent.click(screen.getByLabelText("plus"));
-    });
+    await userEvent.click(screen.getByLabelText("add new preset"));
 
-    await waitFor(() => {
-      expect(addPreset).toHaveBeenCalledWith("chartStylePresets", "차트 서식1");
-    });
+    expect(screen.getByText("차트 서식1")).toBeInTheDocument();
   });
-
-  it("should create a new chart style preset with incremented number", async () => {
-    OfficeRuntime.storage.getItem.mockResolvedValue(
-      JSON.stringify({ "차트 서식1": {}, "차트 서식2": {} }),
-    );
-
+  it("프리셋이 이미 존재할 경우, 프리셋을 추가할 경우 숫자가 1 증가해야합니다.", async () => {
     render(<ChartStyle />);
 
-    await waitFor(() => {
-      expect(OfficeRuntime.storage.getItem).toHaveBeenCalledWith(
-        "chartStylePresets",
-      );
-    });
+    await userEvent.click(screen.getByLabelText("add new preset"));
 
-    fireEvent.click(screen.getByLabelText("plus"));
-
-    await waitFor(() => {
-      expect(addPreset).toHaveBeenCalledWith("chartStylePresets", "차트 서식3");
-    });
+    expect(screen.getByText("차트 서식2")).toBeInTheDocument();
   });
 
-  it("should handle chart style preset deletion", async () => {
-    deletePreset.mockResolvedValue();
-
-    const { rerender } = render(<ChartStyle />);
-
-    act(() => {
-      fireEvent.click(screen.getByLabelText("plus"));
-    });
-
-    await waitFor(() => {
-      expect(addPreset).toHaveBeenCalledWith("chartStylePresets", "차트 서식1");
-    });
-
-    rerender(<ChartStyle />);
-
-    fireEvent.click(screen.getByLabelText("delete"));
-
-    await waitFor(() => {
-      expect(deletePreset).toHaveBeenCalledWith(
-        "chartStylePresets",
-        "차트 서식1",
-      );
-      expect(OfficeRuntime.storage.getItem).toHaveBeenCalledWith(
-        "chartStylePresets",
-      );
-    });
-  });
-
-  it("should save the current chart style preset", async () => {
+  it("프리셋을 삭제할 수 있어야 한다.", async () => {
     render(<ChartStyle />);
 
-    fireEvent.click(screen.getByLabelText("save"));
+    await userEvent.click(screen.getByLabelText("delete preset"));
 
-    await waitFor(() => {
-      expect(copyChartStyle).toHaveBeenCalledWith("chartStylePresets", "");
-    });
+    expect(await loadStorage()).toStrictEqual({ "차트 서식2": {} });
   });
 
-  it("should load the selected chart style preset", async () => {
+  it("현재 서식을 저장할 수 있어야 한다.", async () => {
     render(<ChartStyle />);
 
-    fireEvent.click(screen.getByText("적용"));
+    await userEvent.click(screen.getByLabelText("save button"));
 
-    await waitFor(() => {
-      expect(pasteChartStyle).toHaveBeenCalledWith("chartStylePresets", "");
-    });
+    expect(await loadStorage()).toStrictEqual(savedChartStyle);
   });
+
+  // it("should load the selected chart style preset", async () => {
+  //   render(<ChartStyle />);
+
+  //   await userEvent.click(screen.getByLabelText("paste button"));
+
+  //   expect(mockData.context.workbook._chartType.format).toStrictEqual(
+  //     savedChartStyle["차트 서식1"],
+  //   );
+  // });
 });
